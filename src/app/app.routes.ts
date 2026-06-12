4aa5baa33c7553abb71c2f2cab038e368a1064e2
besnik <besnikbeka2001@gmail.com>
2026-06-12 15:45:04 +0200

    Initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { LocationComponent } from './components/location/location.component';
+import { CareersComponent } from './components/careers/careers.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'', component:HomeComponent},
+    {path:'about', component: AboutComponent},
+    {path:'locations', component: LocationComponent},
+    {path:'careers', component: CareersComponent},
+    {path:'**', redirectTo: '', pathMatch: 'full'}
+
+
+];
