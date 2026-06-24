d9c4a08ab8a28794f1464fb945531decda0efe66
besnik <besnikbeka2001@gmail.com>
2026-06-24 16:52:58 +0200

    update

--- src/app/routes/about/about.component.ts
+++ src/app/routes/about/about.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { SecondaryHeroComponent } from '../../components/secondary-hero/secondary-hero.component';
+import { AboutContentComponent } from '../../components/about-content/about-content.component';
+
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [SecondaryHeroComponent],
+  imports: [SecondaryHeroComponent, AboutContentComponent],
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss'
 })
